b42cee93ffbc74fc2eb474b5c904c5464f2eebcc
meDracula <me.dracula@protonmail.com>
2021-11-05 20:25:57 +0100

    Remove tileset.png redundant and pep-8 settings.py

--- settings.py
+++ settings.py
@@ -8,29 +8,29 @@ GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 MAGENTA = (255, 0, 255)
 
-#Team color Orange:
+# Team color Orange:
 BLUE = (0, 0, 255)
 PLANE_BLUE = (51, 51, 255)
 
-#Team color Orange:
+# Team color Orange:
 ORANGE = (255, 128, 0)
 PLANE_ORANGE = (255, 153, 51)
 
 # game settings
-WIDTH = 1024 # 16 * 64 or 32 * 32 or 64 * 16 -> Tile size * map Tiles
-HEIGHT = 768 # 16 * 48 or 32 * 24 or 64 * 12 -> Tile size * map Tiles
+WIDTH = 1024  # 16 * 64 or 32 * 32 or 64 * 16 -> Tile size * map Tiles
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12 -> Tile size * map Tiles
 FPS = 30
 TITLE = "Maniac Hive"
 BGCOLOR = DARKGREY
-STARTING_TEAM = 'orange' #orange or blue
+STARTING_TEAM = 'orange'  # orange or blue
 
 SPRITESHEET = 'assets/TileSet.png'
-TILESIZE = 64 #64, 32, 16
+TILESIZE = 64  # 64, 32, 16
 GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 TILESIZE_OFFSET = 10
 
 WIN_PERCENTAGE = 0.65
 
-#Directory for player scripts
+# Directory for player scripts
 DIRECTORY = 'scripts'
